50f7663787cb7919340af09116deb4deb9756677
Abdullah Zyoud <aboodzyoud.1997@gmail.com>
2023-08-06 16:52:25 +0300

    initial commit

---
+++ React/First React With JSX/first_jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello Dojo!
+        <ul>Things I need to do :
+          <li>Learn React</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
