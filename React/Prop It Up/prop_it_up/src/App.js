ddcf771580b754cdf4d5a22e4e2c41f07425c498
Abdullah Zyoud <aboodzyoud.1997@gmail.com>
2023-08-06 18:41:56 +0300

    add

---
+++ React/Prop It Up/prop_it_up/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <PersonCard firstName={"ahmad"} lastName={"zyoud"} age={20} hairColor={"red"}/>
+      <PersonCard firstName={"abd"} lastName={"jarrar"} age={30} hairColor={"black"}/>
+      <PersonCard firstName={"samer"} lastName={"abo ghos"} age={25} hairColor={"brawn"}/>
+    </div>
+  );
+}
+
+export default App;
